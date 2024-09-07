5bea3f73babad2280ebf373dd0e5d35b550158be
NathSantos <nathsantos2703@gmail.com>
2024-09-07 14:51:16 -0400

    Componente de Input

--- 3_IgniteGym/src/screens/SignIn.tsx
+++ 3_IgniteGym/src/screens/SignIn.tsx
@@ -1,6 +1,7 @@
-import { VStack, Image, Center, Text } from '@gluestack-ui/themed';
+import { VStack, Image, Center, Text, Heading } from '@gluestack-ui/themed';
 import BackgroundImg from '@assets/background.png';
 import Logo from '@assets/logo.svg';
+import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
@@ -13,13 +14,21 @@ export function SignIn() {
         alt='Pessoas treinando'
         position='absolute'
       />
+      <VStack flex={1} px='$10' pb='$16'>
+        <Center my='$24'>
+          <Logo />
+          <Text fontSize='$sm' color='$gray100'>
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
 
-      <Center my='$24'>
-        <Logo />
-        <Text fontSize='$sm' color='$gray100'>
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
+        <Center gap='$2'>
+          <Heading color='$gray100'>Acesse a conta</Heading>
+
+          <Input placeholder='Email' />
+          <Input placeholder='Senha' />
+        </Center>
+      </VStack>
     </VStack>
   );
 }
