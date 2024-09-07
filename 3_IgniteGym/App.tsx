dd17b1f17c592bf0fc3bb3d118357d28ed599307
NathSantos <nathsantos2703@gmail.com>
2024-09-07 07:38:38 -0400

    Instalando o Gluestack

--- 3_IgniteGym/App.tsx
+++ 3_IgniteGym/App.tsx
@@ -1,9 +1,10 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto';
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,20 +13,22 @@ export default function App() {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#202024',
+        }}
+      >
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        {fontsLoaded ? <Text>Home</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
   );
 }
