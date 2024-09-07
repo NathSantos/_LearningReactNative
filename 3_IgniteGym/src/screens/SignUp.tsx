4cf4d7f1c7d2e737e2b8666195ebb7645566cde7
NathSantos <nathsantos2703@gmail.com>
2024-09-07 15:29:21 -0400

    Criando a SignUp

---
+++ 3_IgniteGym/src/screens/SignUp.tsx
@@ -0,0 +1,54 @@
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+} from '@gluestack-ui/themed';
+import BackgroundImg from '@assets/background.png';
+import Logo from '@assets/logo.svg';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+export function SignUp() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator
+    >
+      <VStack flex={1} bg='$gray700'>
+        <Image
+          source={BackgroundImg}
+          w='$full'
+          h={624}
+          defaultSource={BackgroundImg}
+          alt='Pessoas treinando'
+          position='absolute'
+        />
+        <VStack flex={1} px='$10' pb='$16'>
+          <Center my='$24'>
+            <Logo />
+            <Text fontSize='$sm' color='$gray100'>
+              Treine sua mente e seu corpo
+            </Text>
+          </Center>
+
+          <Center gap='$2' flex={1}>
+            <Heading color='$gray100'>Crie sua conta</Heading>
+
+            <Input placeholder='Nome' />
+            <Input
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCapitalize='none'
+            />
+            <Input placeholder='Senha' secureTextEntry />
+            <Button title='Criar e acessar' />
+          </Center>
+          <Button title='Voltar para o login' variant='outline' mt='$12' />
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+}
