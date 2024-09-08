6c8a5a334c688d74f0391af36b46d6b9000053a8
NathSantos <nathsantos2703@gmail.com>
2024-09-07 22:56:52 -0400

    Finalizando a Interface de Exercícios

--- 3_IgniteGym/src/screens/Exercise.tsx
+++ 3_IgniteGym/src/screens/Exercise.tsx
@@ -5,13 +5,17 @@ import {
   VStack,
   Text,
   Image,
+  Box,
 } from '@gluestack-ui/themed';
 import { useNavigation } from '@react-navigation/native';
 import { ArrowLeft } from 'lucide-react-native';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ScrollView } from 'react-native';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
 import BodySvg from '@assets/body.svg';
+import SeriesSvg from '@assets/series.svg';
+import RepetitionsSvg from '@assets/repetitions.svg';
+import { Button } from '@components/Button';
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -50,19 +54,49 @@ export function Exercise() {
         </HStack>
       </VStack>
 
-      <VStack p='$8'>
-        <Image
-          source={{
-            uri: 'https://image.tuasaude.com/media/article/ll/ae/puxada-frontal_63648_l.jpg',
-          }}
-          alt='Exercício'
-          mb='$3'
-          resizeMode='cover'
-          rounded='$lg'
-          w='$full'
-          h='$80'
-        />
-      </VStack>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 32 }}
+      >
+        <VStack p='$8'>
+          <Image
+            source={{
+              uri: 'https://image.tuasaude.com/media/article/ll/ae/puxada-frontal_63648_l.jpg',
+            }}
+            alt='Exercício'
+            mb='$3'
+            resizeMode='cover'
+            rounded='$lg'
+            w='$full'
+            h='$80'
+          />
+
+          <Box bg='$gray600' rounded='$md' pb='$4' px='$4'>
+            <HStack
+              alignItems='center'
+              justifyContent='space-around'
+              mb='$6'
+              mt='$5'
+            >
+              <HStack alignItems='center'>
+                <SeriesSvg />
+                <Text color='$gray200' ml='$2'>
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack alignItems='center'>
+                <RepetitionsSvg />
+                <Text color='$gray200' ml='$2'>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title='Marcar como realizado' />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
