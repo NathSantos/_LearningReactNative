e349e59cea79d5b803a300852405e8d3ceadef32
NathSantos <nathsantos2703@gmail.com>
2024-09-07 22:30:31 -0400

    Foto Do Usuário

--- 3_IgniteGym/src/screens/Profile.tsx
+++ 3_IgniteGym/src/screens/Profile.tsx
@@ -1,11 +1,23 @@
-import { VStack } from '@gluestack-ui/themed';
+import { VStack, Center } from '@gluestack-ui/themed';
+import { ScrollView } from 'react-native';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt='$6' px='$10'>
+          <UserPhoto
+            source={{ uri: 'https://github.com/NathSantos.png' }}
+            size='xl'
+            alt='Imagem do usuário'
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
