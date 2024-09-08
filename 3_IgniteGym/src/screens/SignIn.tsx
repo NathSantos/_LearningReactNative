4c667253e33a8c224afdde2bd51c30917d346da1
NathSantos <nathsantos2703@gmail.com>
2024-09-07 20:00:42 -0400

    Modificando o Tema da Navegação e evitando glitch na tela

--- 3_IgniteGym/src/screens/SignIn.tsx
+++ 3_IgniteGym/src/screens/SignIn.tsx
@@ -17,7 +17,7 @@ export function SignIn() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator
     >
-      <VStack flex={1} bg='$gray700'>
+      <VStack flex={1}>
         <Image
           source={BackgroundImg}
           w='$full'
--- 3_IgniteGym/src/screens/SignUp.tsx
+++ 3_IgniteGym/src/screens/SignUp.tsx
@@ -17,7 +17,7 @@ export function SignUp() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator
     >
-      <VStack flex={1} bg='$gray700'>
+      <VStack flex={1}>
         <Image
           source={BackgroundImg}
           w='$full'
