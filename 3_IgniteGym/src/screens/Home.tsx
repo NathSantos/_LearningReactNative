6c02f583332bbf77c23356208c8b860656f3973c
NathSantos <nathsantos2703@gmail.com>
2024-09-07 22:10:42 -0400

    Listando Exercícios

--- 3_IgniteGym/src/screens/Home.tsx
+++ 3_IgniteGym/src/screens/Home.tsx
@@ -7,6 +7,14 @@ import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home() {
+  const [exercises, setExercises] = useState<string[]>([
+    'Puxada Frontal',
+    'Remada Curvada',
+    'Remada Unilateral',
+    'Levantamento Terra',
+    'Barra Fixa',
+    'Agachamento Livre',
+  ]);
   const [groups, setGroups] = useState<string[]>([
     'Costas',
     'Bíceps',
@@ -34,17 +42,23 @@ export function Home() {
         style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
       />
 
-      <VStack px='$8'>
+      <VStack px='$8' flex={1}>
         <HStack justifyContent='space-between' mb='$5' alignItems='center'>
           <Heading color='$gray200' fontSize='$md' fontFamily='$heading'>
             Exercícios
           </Heading>
           <Text color='$gray200' fontSize='$sm' fontFamily='$body'>
-            4
+            {exercises.length}
           </Text>
         </HStack>
 
-        <ExerciseCard />
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
       </VStack>
     </VStack>
   );
