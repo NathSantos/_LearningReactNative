e2d705ba726ef53bea02858f08ecbae50cc2ab70
NathSantos <nathsantos2703@gmail.com>
2024-09-07 21:47:06 -0400

    Listando os Grupos

--- 3_IgniteGym/src/screens/Home.tsx
+++ 3_IgniteGym/src/screens/Home.tsx
@@ -1,28 +1,37 @@
 import { HStack, VStack } from '@gluestack-ui/themed';
 import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
+  const [groups, setGroups] = useState<string[]>([
+    'Costas',
+    'Bíceps',
+    'Tríceps',
+    'Ombro',
+  ]);
   const [groupSelected, setGroupSelected] = useState<string>('Costas');
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name='Costas'
-          isActive={groupSelected === 'Costas'}
-          onPress={() => setGroupSelected('Costas')}
-        />
 
-        <Group
-          name='Ombro'
-          isActive={groupSelected === 'Ombro'}
-          onPress={() => setGroupSelected('Ombro')}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
   );
 }
