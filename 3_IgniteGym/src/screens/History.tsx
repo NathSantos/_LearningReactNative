70047503ff6c7a32159afc202fc8090cd34dadb3
NathSantos <nathsantos2703@gmail.com>
2024-09-07 22:25:33 -0400

    Utilizando SectionList

--- 3_IgniteGym/src/screens/History.tsx
+++ 3_IgniteGym/src/screens/History.tsx
@@ -1,13 +1,47 @@
-import { VStack } from '@gluestack-ui/themed';
+import { Heading, VStack, Text } from '@gluestack-ui/themed';
+import { SectionList } from 'react-native';
+import { useState } from 'react';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '22.08.24',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '23.08.24',
+      data: ['Puxada frontal'],
+    },
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Histórico' />
-      <HistoryCard />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color='$gray200' fontSize='$md' mt='$10' mb='$3'>
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color='$gray200' textAlign='center'>
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer execícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </VStack>
   );
 }
