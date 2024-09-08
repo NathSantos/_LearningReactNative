339d66a8d9a6e15e3e5e4d983be21172addd034f
NathSantos <nathsantos2703@gmail.com>
2024-09-08 07:15:19 -0400

    Inputs com estados

--- 3_IgniteGym/src/screens/SignUp.tsx
+++ 3_IgniteGym/src/screens/SignUp.tsx
@@ -7,6 +7,7 @@ import {
   ScrollView,
 } from '@gluestack-ui/themed';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
 
 import BackgroundImg from '@assets/background.png';
 import Logo from '@assets/logo.svg';
@@ -14,10 +15,19 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  function handleSignUp() {
+    console.log({ name, email, password, passwordConfirm });
   }
 
   return (
@@ -45,14 +55,25 @@ export function SignUp() {
           <Center gap='$2' flex={1}>
             <Heading color='$gray100'>Crie sua conta</Heading>
 
-            <Input placeholder='Nome' />
+            <Input placeholder='Nome' onChangeText={setName} />
             <Input
               placeholder='E-mail'
               keyboardType='email-address'
               autoCapitalize='none'
+              onChangeText={setEmail}
             />
-            <Input placeholder='Senha' secureTextEntry />
-            <Button title='Criar e acessar' />
+            <Input
+              placeholder='Senha'
+              secureTextEntry
+              onChangeText={setPassword}
+            />
+            <Input
+              placeholder='Confirme a Senha'
+              secureTextEntry
+              onChangeText={setPasswordConfirm}
+            />
+
+            <Button title='Criar e acessar' onPress={handleSignUp} />
           </Center>
           <Button
             title='Voltar para o login'
