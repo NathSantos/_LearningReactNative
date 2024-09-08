ae91cd4f881acbc603a823f3dd9deabe079d4bc6
NathSantos <nathsantos2703@gmail.com>
2024-09-08 07:43:39 -0400

    Estilizando Mensagens de Validação

--- 3_IgniteGym/src/components/Input.tsx
+++ 3_IgniteGym/src/components/Input.tsx
@@ -1,32 +1,50 @@
-import { Input as GlueStackInput, InputField } from '@gluestack-ui/themed';
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  Input as GluestackInput,
+  InputField,
+} from '@gluestack-ui/themed';
 import { ComponentProps } from 'react';
 
 type Props = ComponentProps<typeof InputField> & {
   isReadOnly?: boolean;
+  errorMessage?: string | null;
+  isInvalid?: boolean;
 };
 
-export function Input({ isReadOnly = false, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  errorMessage = null,
+  isInvalid = false,
+  ...props
+}: Props) {
+  const invalid = !!errorMessage || isInvalid;
   return (
-    <GlueStackInput
-      bg='$gray700'
-      h='$14'
-      borderWidth='$0'
-      borderRadius='$md'
-      $focus={{
-        borderWidth: 1,
-        borderColor: '$green500',
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        px='$4'
-        bg='$gray700'
-        color='$white'
-        fontFamily='$body'
-        placeholderTextColor='$gray300'
-        {...rest}
-      />
-    </GlueStackInput>
+    <FormControl isInvalid={invalid} mb='$4' w='$full'>
+      <GluestackInput
+        h='$14'
+        borderWidth='$0'
+        borderRadius='$md'
+        $focus={{ borderWidth: 1, borderColor: '$green500' }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          px='$4'
+          bg='$gray700'
+          color='$white'
+          fontFamily='$body'
+          placeholderTextColor='$gray300'
+          {...props}
+        />
+      </GluestackInput>
+
+      <FormControlError>
+        <FormControlErrorText color='$red500'>
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
--- 3_IgniteGym/src/screens/SignUp.tsx
+++ 3_IgniteGym/src/screens/SignUp.tsx
@@ -79,12 +79,10 @@ export function SignUp() {
                   placeholder='Nome'
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.name?.message}
                 />
               )}
             />
-            {errors.name?.message && (
-              <Text color='$white'>{errors.name?.message}</Text>
-            )}
 
             <Controller
               control={control}
@@ -103,12 +101,10 @@ export function SignUp() {
                   autoCapitalize='none'
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.email?.message}
                 />
               )}
             />
-            {errors.email?.message && (
-              <Text color='$white'>{errors.email?.message}</Text>
-            )}
 
             <Controller
               control={control}
