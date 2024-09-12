3222a0fac06bf2d35ec9d4235ea2535ac7923629
NathSantos <nathsantos2703@gmail.com>
2024-09-12 16:29:51 -0400

    Contextos no React Native - Exibindo nome e foto do usuário

--- 3_IgniteGym/src/components/HomeHeader.tsx
+++ 3_IgniteGym/src/components/HomeHeader.tsx
@@ -1,13 +1,17 @@
 import { Heading, HStack, Text, VStack, Icon } from '@gluestack-ui/themed';
 import { LogOut } from 'lucide-react-native';
 
+import defaulUserPhotoImg from '@assets/userPhotoDefault.png';
+import { useAuth } from '@hooks/useAuth';
 import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg='$gray600' pt='$16' pb='$5' px='$8' alignItems='center' gap='$4'>
       <UserPhoto
-        source={{ uri: 'https://github.com/NathSantos.png' }}
+        source={user.avatar ? { uri: user.avatar } : defaulUserPhotoImg}
         w='$16'
         h='$16'
         alt='Imagem do Usuário'
@@ -17,7 +21,7 @@ export function HomeHeader() {
           Olá,
         </Text>
         <Heading color='$gray100' fontSize='$md'>
-          Nathália
+          {user.name}
         </Heading>
       </VStack>
 
