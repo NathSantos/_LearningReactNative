dad5970202189480eaa86be70614c2967bb0aaa3
NathSantos <nathsantos2703@gmail.com>
2024-09-12 16:11:58 -0400

    Contextos no React Native - Redirecionar usuário logado

--- 3_IgniteGym/src/routes/index.tsx
+++ 3_IgniteGym/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Box } from '@gluestack-ui/themed';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
 import { gluestackUIConfig } from '../../config/gluestack-ui.config';
 import { useAuth } from '@hooks/useAuth';
 
@@ -11,12 +12,10 @@ export function Routes() {
 
   const { user } = useAuth();
 
-  console.log('USUÁRIO LOGADO =>', user);
-
   return (
     <Box flex={1} bg='$gray700'>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
